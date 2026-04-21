b54dfe615bb899522cefdf60272f6936ff1f9ec4
LaPI-juan <juan.saldana2504@gmail.com>
2026-04-21 13:22:40 -0600

    Add files via upload

---
+++ App_prueba.py
@@ -0,0 +1,142 @@
+import streamlit as st
+import SimpleITK as sitk
+import tempfile
+import os
+import textwrap
+from PIL import Image
+
+#### Funciones propias #### 
+from RotarVolumen import leer_archivos_dicom
+from conversor import carpetaPNG
+
+#### Estilo HTML ####
+
+#### Tarjeta #### 
+st.markdown('''
+        <style>
+        .card {
+            padding: 15px;
+            background-color: #f8f9fa;
+            border-radius: 12px;
+            border: 1px solid #ddd;
+            margin-top: 10px;
+        }
+        .card h4 {
+            margin-top: 0;
+        }
+        </style>
+        ''', unsafe_allow_html=True)
+
+#### Inicializar #### 
+if 'screen' not in st.session_state:
+    st.session_state.screen = 1
+
+def go_welcome():
+    st.session_state.screen = 1
+
+def go_lobby():
+    st.session_state.screen = 2
+
+#### Presentacion #### 
+if st.session_state.screen == 1:
+    st.title('Welcome')
+    st.markdown('Press **Start** to enter')
+    st.button('Start ▶️', on_click = go_lobby)
+	
+#### Lobby #### 
+elif st.session_state.screen == 2:
+
+    st.set_page_config(layout='wide')
+	
+    upload_dcm_1 = st.sidebar.file_uploader('**Archivos DCM 1**', type=['DCM'],
+                                            accept_multiple_files=True, key='dcm1')
+    
+    upload_dcm_2 = st.sidebar.file_uploader('**Archivos DCM 2**', type=['DCM'],
+                                            accept_multiple_files=True, key='dcm2')
+
+    upload_dcm_3 = st.sidebar.file_uploader('**Archivos DCM 3**', type=['DCM'],
+                                            accept_multiple_files=True, key='dcm3')
+
+    upload_dcm_4 = st.sidebar.file_uploader('**Archivos DCM 4**', type=['DCM'],
+                                            accept_multiple_files=True, key='dcm4')
+
+    upload_dcm_5 = st.sidebar.file_uploader('**Archivos DCM 5**', type=['DCM'],
+                                            accept_multiple_files=True, key='dcm5')
+
+    upload_dcm_6 = st.sidebar.file_uploader('**Archivos DCM 6**', type=['DCM'],
+                                            accept_multiple_files=True, key='dcm6')
+    
+    upload_dcm_7 = st.sidebar.file_uploader('**Archivos DCM 7**', type=['DCM'],
+                                            accept_multiple_files=True, key='dcm7')
+
+    upload_dcm_8 = st.sidebar.file_uploader('**Archivos DCM 8**', type=['DCM'],
+                                            accept_multiple_files=True, key='dcm8')
+
+    upload_dcm_9 = st.sidebar.file_uploader('**Archivos DCM 9**', type=['DCM'],
+                                            accept_multiple_files=True, key='dcm9')
+
+    upload_dcm_10 = st.sidebar.file_uploader('**Archivos DCM 10**', type=['DCM'],
+                                            accept_multiple_files=True, key='dcm10')
+
+    upload_dcm_11 = st.sidebar.file_uploader('**Archivos DCM 11**', type=['DCM'],
+                                            accept_multiple_files=True, key='dcm11')
+    
+    if upload_dcm_1:
+        
+        upload_dcms = [upload_dcm_1,upload_dcm_2,upload_dcm_3,upload_dcm_4,upload_dcm_5,
+                       upload_dcm_6,upload_dcm_7,upload_dcm_8,upload_dcm_9,upload_dcm_10,
+                       upload_dcm_11]
+        
+        st.write(len(upload_dcms))
+        # ------------------------------------------------------------------------------------
+        #                                   SUBIDA DE DATOS
+        # ------------------------------------------------------------------------------------
+		#### Carpeta temporal DICOM ####
+        if 'rutas_DCM' not in st.session_state:
+            rutas_DCM = []
+
+            for upload_dcm in upload_dcms:
+                temp_dcm_org = tempfile.mkdtemp()
+                
+                for f in upload_dcm:
+                    path = os.path.join(temp_dcm_org, f.name)
+                    with open(path, 'wb') as out:
+                        out.write(f.getbuffer())
+
+                rutas_DCM.append(temp_dcm_org)
+
+            st.session_state.rutas_DCM = rutas_DCM
+
+        rutas_DCM = st.session_state.rutas_DCM
+
+        # ------------------------------------------------------------------------------------
+        #                                      ORIGINAL
+        # ------------------------------------------------------------------------------------
+
+        #### Volumenes ####
+        if 'HV_org' not in st.session_state:
+            st.session_state.HV_org = leer_archivos_dicom_mult(rutas_DCM)
+
+        HV_org = st.session_state.HV_org
+
+		#### Carpetas temporal PNG ####
+        if 'temp_png_orgs' not in st.session_state:
+            
+            st.session_state.temp_png_orgs = [carpetaPNG(V_org,0) for V_org in HV_org]
+
+        temp_png_orgs = st.session_state.temp_png_orgs
+
+
+        html_3 = f'''
+            <div class="card">
+                <h4>Imagenes</h4>
+            </div>
+        '''
+        st.markdown(textwrap.dedent(html_3), unsafe_allow_html=True)
+        
+        N_org_1 = st.slider('Volumen',min_value=1, max_value=len(HV_org), step=1,key ='N_org_1')
+        N_org_2 = st.slider('Volumen',min_value=1, max_value=HV_org[0].shape[0], step=1,key ='N_org_2')
+
+        img_orig_user_1 = Image.open(os.path.join(temp_png_orgs[N_org_1-1], f'slice_{(N_org_2-1):03d}.png'))
+
+        st.image(img_orig_user_1, caption='Original', use_container_width=True)
