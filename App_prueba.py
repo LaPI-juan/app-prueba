20b75085325c1f13a5caa1e2289c41f55bd95a51
LaPI-juan <juan.saldana2504@gmail.com>
2026-04-23 19:07:39 -0600

    Update App_prueba.py

--- App_prueba.py
+++ App_prueba.py
@@ -205,9 +205,15 @@ elif st.session_state.screen == 2:
 
         temp_png_valvs = st.session_state.temp_png_valvs
         st.write('Bandera')
-        st.write(len(temp_png_valvs))
-        st.write(len(HV_org))
 
+        # ------------------------------------------------------------------------------------
+        #                                    YOLO
+        # ------------------------------------------------------------------------------------
+        if 'temp_png_YOLOs' not in st.session_state:
+            HV_YOLO = [cargarVolumen_YOLO(ruta) for ruta in temp_png_valvs]
+            st.session_state.temp_png_YOLOs  = [carpetaPNG(V_YOLO[:,:,:,0],0) for V_YOLO in HV_YOLO]
+        temp_png_YOLOs = st.session_state.temp_png_YOLOs
+		
         tab1, tab2, tab3 = st.tabs(['Estándar', 'LVOT', 'Mascara'])
 
         # ------------------------------------------------------------------------------------
