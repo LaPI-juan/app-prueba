1632ecd216c437eef2ff72a503ac682405a50c14
LaPI-juan <juan.saldana2504@gmail.com>
2026-04-23 20:25:13 -0600

    Update App_prueba.py

--- App_prueba.py
+++ App_prueba.py
@@ -206,11 +206,12 @@ elif st.session_state.screen == 2:
         # ------------------------------------------------------------------------------------
         #                                    YOLO
         # ------------------------------------------------------------------------------------
-#        if 'temp_png_YOLOs' not in st.session_state:
-#            HV_YOLO = [CargarVolumen_YOLO(ruta) for ruta in temp_png_valvs]
-#            st.write(HV_YOLO)
+        if 'temp_png_YOLOs' not in st.session_state:
+            HV_YOLO = [CargarVolumen_YOLO(ruta) for ruta in temp_png_valvs]
+            st.session_state.HV_YOLO = HV_YOLO 
 #            st.session_state.temp_png_YOLOs  = [carpetaPNG(V_YOLO[:,:,:,0],0) for V_YOLO in HV_YOLO]
 #        temp_png_YOLOs = st.session_state.temp_png_YOLOs
+        st.session_state.HV_YOLO = HV_YOLO 
 		
         tab1, tab2, tab3 = st.tabs(['Estándar', 'LVOT', 'Mascara'])
 
