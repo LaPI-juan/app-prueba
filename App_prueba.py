3d9a3127083e1c9bcf405d091691a16b2a0ade23
LaPI-juan <juan.saldana2504@gmail.com>
2026-04-26 21:44:06 -0600

    Update App_prueba.py

--- App_prueba.py
+++ App_prueba.py
@@ -224,11 +224,13 @@ elif st.session_state.screen == 2:
             HV_YOLO = [CargarVolumen_YOLO(temp_png_valvs_chico[0])]
             HV_YOLO_NEW = [CargarVolumen_NEW(temp_png_valvs[0])]
             st.session_state.HV_YOLO = HV_YOLO
+            st.session_state.HV_YOLO_NEW = HV_YOLO_NEW
             st.session_state.temp_png_YOLOs = [carpetaPNG(V_YOLO[:,:,:,0],0) for V_YOLO in HV_YOLO]
 
         temp_png_YOLOs = st.session_state.temp_png_YOLOs
         HV_YOLO = st.session_state.HV_YOLO
-        st.write(HV_YOLO[0].shape)
+		HV_YOLO_NEW = st.session_state.HV_YOLO
+        st.write(HV_YOLO_NEW)
 
         tab1, tab2, tab3 = st.tabs(['Estándar', 'LVOT', 'Mascara'])
 
