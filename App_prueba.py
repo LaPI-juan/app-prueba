d4b1ab75a4116c7a10fbde51d3f8a9995b5b6f6f
LaPI-juan <juan.saldana2504@gmail.com>
2026-04-26 20:21:40 -0600

    Update App_prueba.py

--- App_prueba.py
+++ App_prueba.py
@@ -200,12 +200,13 @@ elif st.session_state.screen == 2:
         #### Carpeta temporal PNG ####
         if 'temp_png_valvs' not in st.session_state:
             HV_valv = [[V_LVOT[:,i,:] for i in range(V_LVOT.shape[2])] for V_LVOT in HV_LVOT]
+            st.session_state.HV_valv  = HV_valv 
 #            temp_png_valvs, paths_png_valvs  = carpetaPNG_paths(HV_valv[0] ,0)
 #            st.session_state.temp_png_valvs = temp_png_valvs
 #            st.session_state.paths_png_valvs = paths_png_valvs 
-            st.session_state.temp_png_valvs = [carpetaPNG(V_valv[0],0) for V_valv in HV_valv]
+            st.session_state.temp_png_valvs = [carpetaPNG(V_valv,0) for V_valv in HV_valv]
 
-
+        HV_valv = st.session_state.HV_valv 
         temp_png_valvs = st.session_state.temp_png_valvs
         st.write(HV_valv[0].shape)
 #        paths_png_valvs = st.session_state.paths_png_valvs
