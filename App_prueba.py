4d6f92d1078f34a7d37786d9e9f29672bb3475df
LaPI-juan <juan.saldana2504@gmail.com>
2026-04-26 20:55:34 -0600

    Update App_prueba.py

--- App_prueba.py
+++ App_prueba.py
@@ -225,7 +225,7 @@ elif st.session_state.screen == 2:
             st.session_state.HV_YOLO = HV_YOLO
             st.session_state.temp_png_YOLOs = [carpetaPNG(V_YOLO[:,:,:,0],0) for V_YOLO in HV_YOLO]
 
-        #temp_png_YOLOs = st.session_state.temp_png_YOLOs
+        temp_png_YOLOs = st.session_state.temp_png_YOLOs
         HV_YOLO = st.session_state.HV_YOLO
         st.write(HV_YOLO[0].shape)
 
@@ -246,7 +246,7 @@ elif st.session_state.screen == 2:
         	N_fnl_1 = st.slider('Corte',min_value=1, max_value=HV_org[0].shape[0], step=1,key ='N_fnl1')
 
         	img_orig_user_1 = Image.open(os.path.join(temp_png_valvs[N_org_1-1], f'slice_{(N_fnl_1-1):03d}.png'))
-        	img_fnl_user_1 = Image.open(os.path.join(temp_png_valvs_chico[N_org_1-1], f'slice_{(N_fnl_1-1):03d}.png'))
+        	img_fnl_user_1 = Image.open(os.path.join(temp_png_YOLOs[N_org_1-1], f'slice_{(N_fnl_1-1):03d}.png'))
 
         	col1, col2 = st.columns(2)
         	with col1:
