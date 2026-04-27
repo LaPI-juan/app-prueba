6af4e46fbd49fbaa3035b3b3e56323389e63c5a4
LaPI-juan <juan.saldana2504@gmail.com>
2026-04-27 13:21:22 -0600

    Update App_prueba.py

--- App_prueba.py
+++ App_prueba.py
@@ -215,14 +215,14 @@ elif st.session_state.screen == 2:
         if 'temp_png_YOLOs' not in st.session_state:
             HV_YOLO = [[CargarVolumen_YOLO(ruta) for ruta in rutas] for rutas in temp_png_valvs_chico]
             st.session_state.HV_YOLO = HV_YOLO
-            st.session_state.hv_YOLO = np.array(HV_YOLO[0])
-            #st.session_state.temp_png_YOLOs = [carpetaPNG(HV_YOLO[0][:,0,:,:,0],0)]
+            hv_YOLO = np.array(HV_YOLO[0])
+            st.session_state.temp_png_YOLOs = [carpetaPNG(hv_YOLO[0][:,0,:,:,0],0)]
 			
         HV_YOLO = st.session_state.HV_YOLO
-        hv_YOLO = st.session_state.hv_YOLO
+        #hv_YOLO = st.session_state.hv_YOLO
         st.write(len(HV_YOLO))
         st.write(len(HV_YOLO[0]))
-        st.write(hv_YOLO.shape)
+        #st.write(hv_YOLO.shape)
         #temp_png_YOLOs = st.session_state.temp_png_YOLOs
         #st.write(len(temp_png_YOLOs))
         #st.write(len(temp_png_YOLOs[0]))
