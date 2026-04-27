fe401978c234d87b478009448ad11d6e560f318d
LaPI-juan <juan.saldana2504@gmail.com>
2026-04-26 22:17:26 -0600

    Update App_prueba.py

--- App_prueba.py
+++ App_prueba.py
@@ -206,7 +206,7 @@ elif st.session_state.screen == 2:
 #            st.session_state.paths_png_valvs = paths_png_valvs 
             st.session_state.temp_png_valvs = [carpetaPNG(V_valv,0) for V_valv in HV_valv]
 #            st.session_state.temp_png_valvs_chico = [carpetaPNG(V_valv[0:4],0) for V_valv in HV_valv]
-            HV_valv_numpy = np.array(HV_valv)
+            HV_valv_numpy = np.array(HV_valv[0])
 #            st.session_state.temp_png_valvs_chico = [carpetaPNG(HV_valv[0],0) for S_valv in HV_valv[0]]
 
         HV_valv = st.session_state.HV_valv 
